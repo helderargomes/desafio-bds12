280217e967bfa118687abc3c5dede711824af225
helderargomes <helderargomes@gmail.com>
2025-03-20 13:26:08 -0300

    Cities retrieval from backend, and select styling update

--- frontweb/src/components/filter/index.tsx
+++ frontweb/src/components/filter/index.tsx
@@ -1,15 +1,48 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
+import { City } from '../../types/city';
+import Select from 'react-select';
+import { BASE_URL, requestBackend } from '../../utils/requests';
+import { Controller, useForm } from 'react-hook-form';
+
+type FilterData = {
+  city: City;
+};
 
 function Filter() {
+  const [selectCities, setSelectCities] = useState<City[]>([]);
+
+  useEffect(() => {
+    requestBackend({ url: `${BASE_URL}/stores` }).then((response) => {
+      setSelectCities(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  const { register, handleSubmit, setValue, control } = useForm<FilterData>();
+
+  const handleChangeCity = (value: City) => {
+    setValue('city', value);
+  };
+
+  const onSubmit = (formData: FilterData) => {
+    console.log(formData);
+  };
+
   return (
     <div className="filter-container base-card">
-      <select className="select-input">
-        <option>Selecione uma cidade</option>
-        <option value="Uberaba">Uberaba</option>
-        <option value="Uberlândia">Uberlândia</option>
-        <option value="Araguari">Araguari</option>
-        <option value="Ituiutaba">Ituiutaba</option>
-      </select>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Select
+            options={selectCities}
+            isClearable
+            placeholder={'Selecione uma cidade'}
+            classNamePrefix="city-filter-select"
+            getOptionLabel={(city: City) => city.name}
+            getOptionValue={(city: City) => String(city.id)}
+          />
+        </div>
+      </form>
     </div>
   );
 }
